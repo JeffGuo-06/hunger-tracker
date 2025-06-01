febbb6b7356930c23bc79135ae0d23348b69ee8f
JeffGuo-06 <jeffguo.06@gmail.com>
2025-05-31 22:08:59 -0400

    splash

--- HungerTracker/app/(onboarding)/hero3.tsx
+++ HungerTracker/app/(onboarding)/hero3.tsx
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
     marginTop: spacing.md,
   },
   buttonText: {
-    color: colors.text[1],
+    color: colors.buttonText,
     fontSize: fontSizes.medium,
     fontWeight: '600',
   },
